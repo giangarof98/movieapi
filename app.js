e6a6430e4ffc84daa957ab84cab63aa0fdc5bb2e
Gianmarco G <giangarof98@gmail.com>
2022-03-11 18:17:32 -0500

    done

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ createAutoComplete({
     root: document.querySelector('#left-autocomplete'),
     onOptionSelect(movie){
         document.querySelector('.tutorial').classList.add('is-hidden')
-        onSelectMovie(movie, document.querySelector('#left-summary'));
+        onSelectMovie(movie, document.querySelector('#left-summary'), 'left');
     },
 });
 
@@ -32,20 +32,69 @@ createAutoComplete({
     root: document.querySelector('#right-autocomplete'),
     onOptionSelect(movie){
         document.querySelector('.tutorial').classList.add('is-hidden')
-        onSelectMovie(movie, document.querySelector('#right-summary'));
+        onSelectMovie(movie, document.querySelector('#right-summary'), 'right');
     },
 });
 
+let leftMovie;
+let rightMovie;
 
-
-const onSelectMovie = async (movie, summaryEl) => {
+const onSelectMovie = async (movie, summaryEl, side) => {
     const res = await axios.get('http://www.omdbapi.com/', {
         params: { apikey: "13e27bcd", i: movie.imdbID}
+    });
+    summaryEl.innerHTML = movieTemplate(res.data);
+
+    if(side === 'left'){
+        leftMovie = res.data;
+    } else{
+        rightMovie = res.data;
+    }
+
+    if(leftMovie && rightMovie){
+        runComparison();
+    }
+};
+
+const runComparison = () => {
+    const leftSideStats = document.querySelectorAll('#left-summary .notification')
+    const rightSideStats = document.querySelectorAll('#right-summary .notification')
+
+    leftSideStats.forEach((leftStat, index) => {
+        const rightStat = rightSideStats[index];
+        const leftSideValue = parseInt(leftStat.dataset.value);
+        const rightSideValue = parseInt(rightStat.dataset.value);
+
+
+        if(rightSideValue > leftSideValue){
+            leftStat.classList.remove('is-primary');
+            leftStat.classList.add('is-warning');
+        } else {
+            rightStat.classList.remove('is-primary');
+            leftStat.classList.add('is-warning');
+        }
     })
-    summaryEl.innerHTML = movieTemplate(res.data)
 }
 
 const movieTemplate = (movieDetail) => {
+    const dollars = parseInt(
+        movieDetail.BoxOffice.replace(/\$/g,'').replace(/,/g,'')
+    )
+
+    const metascore = parseInt(movieDetail.Metascore);
+    const imdbRating = parseFloat(movieDetail.imdbRating);
+    const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g,''))
+    const awards = movieDetail.Awards.split(' ').reduce((prev, word) => {
+        const value = parseInt(word);
+
+        if (isNaN(value)){
+            return prev;
+        } else {
+            return prev + value;
+        }
+    }, 0)
+    console.log(awards)
+
     return `
         <article class="media">
             <figure class="media-left">
@@ -61,23 +110,23 @@ const movieTemplate = (movieDetail) => {
                 </div>
             </div>
         </article>
-        <article class="notification is-primary">
+        <article data-value=${awards} class="notification is-primary">
             <p class="title">${movieDetail.Awards}</p>
             <p class="subtitle">Awards</p>
         </article>
-        <article class="notification is-primary">
+        <article data-value=${dollars} class="notification is-primary">
             <p class="title">${movieDetail.BoxOffice}</p>
             <p class="subtitle">Box Office</p>
         </article>
-        <article class="notification is-primary">
+        <article data-value=${metascore} class="notification is-primary">
             <p class="title">${movieDetail.Metascore}</p>
             <p class="subtitle">Metascore</p>
         </article>
-        <article class="notification is-primary">
+        <article data-value=${imdbRating} class="notification is-primary">
             <p class="title">${movieDetail.imdbRating}</p>
             <p class="subtitle">IMDB Rating</p>
         </article>
-        <article class="notification is-primary">
+        <article data-value=${imdbVotes} class="notification is-primary">
             <p class="title">${movieDetail.imdbVotes}</p>
             <p class="subtitle">IMBD Votes</p>
         </article>
