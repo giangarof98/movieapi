14a59546d31723df18efdf2bb33b341fa1ebb287
Gianmarco G <giangarof98@gmail.com>
2022-03-11 12:04:05 -0500

    refactor reusable code

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const fetchData = async(searchTearm) => {
     const res = await axios.get('http://www.omdbapi.com/', {
         params: { apikey: "13e27bcd", s: searchTearm}
@@ -7,55 +6,16 @@ const fetchData = async(searchTearm) => {
     return res.data.Search;
 }
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-<label><b>Search for a movie</b></label>
-<input class="input" />
-<div class="dropdown">
-    <div class="dropdown-menu">
-        <div class="dropdown-content results"></div>
-    </div>
-</div>
-`
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results')
-
-const onInput = async e => {
-    const movies = await fetchData(e.target.value);
-    
-    if(!movies.length){
-        dropdown.classList.remove('is-active');
-        return;
-    }
-
-    resultsWrapper.innerHTML = '';
-    dropdown.classList.add('is-active')
-    for(let movie of movies){
-        const option = document.createElement('a');
+createAutoComplete({
+    root: document.querySelector('.autocomplete'),
+    renderOption(movie){
         const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-        option.classList.add('dropdown-item')
-        option.innerHTML = ` 
-                <img src="${imgSrc}"/>
-                <h1>${movie.Title}</h1>
+        return ` 
+            <img src="${imgSrc}"/>
+            <h1>${movie.Title} (${movie.Year})</h1>
         `;
-        option.addEventListener('click', () => {
-            dropdown.classList.remove('is-active');
-            input.value = movie.Title;
-            onSelectMovie(movie)
-        })
-        resultsWrapper.appendChild(option)
     }
-}
-
-input.addEventListener('input', debounce(onInput, 500))
-
-document.addEventListener("click", e => {
-    if(!root.contains(e.target)){
-        dropdown.classList.remove('is-active');
-    }
-})
+});
 
 const onSelectMovie = async movie => {
     const res = await axios.get('http://www.omdbapi.com/', {
