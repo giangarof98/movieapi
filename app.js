fea3752018fa7013e59a8e0c338d520c17d7dad0
Gianmarco G <giangarof98@gmail.com>
2022-03-11 09:45:33 -0500

    refactor html inside javascript

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 
 const fetchData = async(searchTearm) => {
     const res = await axios.get('http://www.omdbapi.com/', {
-        params: {
-            apikey: "13e27bcd",
-            s: searchTearm,
-        }
+        params: { apikey: "13e27bcd", s: searchTearm}
     })
+    if(res.data.Error){ return []}
     return res.data.Search;
 }
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+<label><b>Search for a movie</b></label>
+<input class="input" />
+<div class="dropdown">
+    <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+    </div>
+</div>
+`
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results')
 
 const onInput = async e => {
     const movies = await fetchData(e.target.value);
+    
+    dropdown.classList.add('is-active')
     for(let movie of movies){
-        const div = document.createElement('div');
-
-        div.innerHTML = ` 
+        const option = document.createElement('a');
+        option.classList.add('dropdown-item')
+        option.innerHTML = ` 
                 <img src="${movie.Poster}"/>
                 <h1>${movie.Title}</h1>
         `;
-        document.querySelector('#poster').appendChild(div)
+        resultsWrapper.appendChild(option)
     }
 }
 
